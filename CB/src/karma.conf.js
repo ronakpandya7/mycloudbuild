25f3ed4ccde60c7d8354c134a6f6c3e2755d38b2
Michael Ziems <admin@example.com>
2019-04-26 17:18:06 +0530

    changes

--- CB/src/karma.conf.js
+++ CB/src/karma.conf.js
@@ -12,12 +12,6 @@ module.exports = function (config) {
       require('karma-coverage-istanbul-reporter'),
       require('@angular-devkit/build-angular/plugins/karma')
     ],
-    customLaunchers: {
-      ChromeHeadlessCI: {
-        base: 'ChromeHeadless',
-        flags: ['--no-sandbox']
-      }
-    },
     client: {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
@@ -31,7 +25,7 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['ChromeChromeHeadless'],
+    browsers: ['Chrome'],
     singleRun: false
   });
 };
