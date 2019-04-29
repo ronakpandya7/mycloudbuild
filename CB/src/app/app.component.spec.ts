1925e792ea1c51247f947b92d4a35374153db81f
Michael Ziems <admin@example.com>
2019-04-29 16:44:28 +0530

    changes

--- CB/src/app/app.component.spec.ts
+++ CB/src/app/app.component.spec.ts
@@ -30,6 +30,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to mycloudbuild!1');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to mycloudbuild!');
   });
 });
